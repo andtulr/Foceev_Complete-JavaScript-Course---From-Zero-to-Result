c910768a226cf4f0db45dacf3ff60f9f8b1c83d1
tao <andtulr@gmail.com>
2024-11-29 15:11:04 +0300

    29.11.15:10

--- VS_JS/Create a love generator/complete/genLove.js
+++ VS_JS/Create a love generator/complete/genLove.js
@@ -5,8 +5,59 @@
   1) Две переменные  c promt -->
   2) сгенерируем число = переменная 
   3) функция alert  результат совместимости имен в выражение косые кв.   */ 
+
+
+  /*const firstName = prompt("Напишите первое имя", "Моё Имя")
+const secondName = prompt("Напишите второе имя", "Моё Имя")
+const randomNumber = (Math.floor(Math.random() * 100))
+
+alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}`)*/
+
+
+/*------------19. Знакомство с If Else. Условное ветвление --------------
+const firstName = prompt("Напишите первое имя", "Моё Имя")
+const secondName = prompt("Напишите второе имя", "Моё Имя")
+const randomNumber = (Math.floor(Math.random() * 100))
+if (randomNumber < 33) {
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Вам необходимо поискать человека с другим именем`) 
+} else if (randomNumber >= 33 && randomNumber < 66 ){
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Совместимость хорошая, но может быть и лучше`)
+} else if (randomNumber >= 66 ) {
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Вы идеально подходите друг другу`)
+} else {
+  alert ("Упс ... Какое-то странное число получилось. Попробуйте ещё раз.")
+}
+*/
+
+/*-------20. Как работают логические операторы !  &&-------- 
 const firstName = prompt("Напишите первое имя", "Моё Имя")
 const secondName = prompt("Напишите второе имя", "Моё Имя")
 const randomNumber = (Math.floor(Math.random() * 100))
 
-alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}`)
+if (randomNumber < 33 && firstName !=null && secondName !=null) {
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Вам необходимо поискать человека с другим именем`) 
+} else if (randomNumber >= 33 && randomNumber < 66 && firstName !=null && secondName !=null ){
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Совместимость хорошая, но может быть и лучше`)
+} else if (randomNumber >= 66 && firstName !=null && secondName !=null) {
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Вы идеально подходите друг другу`)
+} else {
+  alert ("Упс ... Вы не ввели имя. Попробуйте ещё раз.")
+}*/
+
+/*------- вывод не равен пустой строке -------- 
+const firstName = prompt("Напишите первое имя", "Моё Имя")
+const secondName = prompt("Напишите второе имя", "Моё Имя")
+const randomNumber = (Math.floor(Math.random() * 100))
+
+if (randomNumber < 33 && firstName !=null && secondName !=null && firstName !="" && secondName !="" ) {
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Вам необходимо поискать человека с другим именем`) 
+} else if (randomNumber >= 33 && randomNumber < 66 && firstName !=null && secondName !=null && firstName !="" && secondName !="" ){
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Совместимость хорошая, но может быть и лучше`)
+} else if (randomNumber >= 66 && firstName !=null && secondName !=null && firstName !="" && secondName !="" ) {
+  alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Вы идеально подходите друг другу`)
+} else {
+  alert ("Упс ... Вы не ввели имя. Попробуйте ещё раз.")
+}
+*/
+
+
