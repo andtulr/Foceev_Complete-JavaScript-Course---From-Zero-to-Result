63b9e08aafa6d5bfadce2e363543e84e14eb2a2b
tao <andtulr@gmail.com>
2024-12-10 08:09:32 +0300

    10_12_08:10

---
+++ Test/dom.js
@@ -0,0 +1,27 @@
+
+/*
+let h = document.querySelector(".header");
+//console.log(h);
+console.dir(h.firstChild)
+console.log(h.firstChild)
+// console.dir(h)
+/*let l = document.querySelectorAll(".nav__item");
+console.log(l);
+let dE = document.documentElement
+console.log(dE)
+console.dir(dE)
+console.dir(document.head)
+let navList = document.querySelector(".nav__list")
+console.log(navList.childNodes);
+let navList = document.querySelector(".nav__list").childNodes
+console.log(navList)
+
+let navItems = document.querySelectorAll("li");
+console.log(navItems);
+console.log(navItems[1]);
+console.log(navItems.length);
+*/
+let elem = document.querySelector("h1");
+console.log(elem.innerHTML);
+elem.innerHTML = "Hello Irina" // Hello Andrei - Hello Irina
+
