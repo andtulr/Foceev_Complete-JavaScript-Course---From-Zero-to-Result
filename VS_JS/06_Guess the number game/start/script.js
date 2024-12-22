b200e1097112a7aa195cc5bb59cd9d0fab6bc4a6
tao <andtulr@gmail.com>
2024-12-22 08:31:28 +0300

    22_12_08:35

--- VS_JS/06_Guess the number game/start/script.js
+++ VS_JS/06_Guess the number game/start/script.js
@@ -12,27 +12,55 @@
 
 let sN = Math.trunc(Math.random() * 20) + 1; // Math.trunc-округление рондомного числа(Math.random()-получение РЧ*20)
 //console.log(sN)
-let scor = 20; // счёт 20
-const disMes = function(message) {
+let score = 20; // счёт 20
+let hS = 0;
+const disMes = function(message) { // при определённом действие в окне "проверка" - меняется текст
     document.querySelector(".message").textContent = message // вызываем класс .message - к свойсву textContent - присваеваем параметр значение функции(message)
 }
 // Логика - 1) Кнопка"Проверка"-запускает"Начните угадывать"
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value);  // вызываем класс (".quess").value-знечение--поле для ввода вводим значение.Выводит строку-переводим в цифру-Number()или+
+    
     if (!guess) {
-        //document.querySelector(".message").textContent = "Вы не ввели число!";
+        // document.querySelector(".message").textContent = "Вы не ввели число!";
         disMes("Вы не ввели число!");
-    }else if (guess === sN) {
-        disMes("Вы победили!");
-        document.querySelector("body").style.background = "green";
-        document.querySelector(".number").textContent = guess;
-    }else if (guess !== sN) {
-        
+        }else if (guess === sN) { // при совподение чисел - действия ниже
+            disMes("Вы победили!");
+            document.querySelector("body").style.background = "green";
+            document.querySelector(".number").textContent = guess;
+            if(score > hS) {
+                hS = score
+                document.querySelector(".highscore").textContent = hS;
+            }
+
+    }else if (guess !== sN) { // не равен 
+        if(score > 1) {
+            if(guess > sN) {
+                disMes("Число больше");
+                score--;
+                document.querySelector(".score").textContent = score;  
+            } else if (guess < sN) {
+                disMes("Число меньше");
+                score--;
+                document.querySelector(".score").textContent = score;
+            } 
+              
+        } else {
+            disMes("Вы проиграли!");
+            document.querySelector(".score").textContent = 0;
+        }
     }
     
-    
 });
-
+// кнопка снова
+document.querySelector(".again").addEventListener("click", function () {
+    score = 20;
+    sN = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".guess").value = "";
+    document.querySelector("body").style.background = "black";
+})
 
 
 
--- VS_JS/07_Create a modal window/start/script.js
+++ VS_JS/07_Create a modal window/start/script.js
@@ -11,3 +11,35 @@
 2-При добавлении класса .hidden к любому элементу, он исчезнет, при удалении этого класса, он появится
 3-Не забудьте про то, что закрыть модальное окно можно как с помощью кнопки-крестика, в верхнем правом углу модального окна, так и с помощью нажатия на любое место "Оверлей"
 */
+const btnOpenMod = document.querySelectorAll(".show-modal"); // кнопки для включения всех модальных окон(псевдо колекция)
+const modWin = document.querySelector(".modal"); // модальное окно выходящее
+const btnCloseMod = document.querySelector(".close-modal"); // крестик на модальном окне
+const overlay = document.querySelector(".overlay"); //затемненный, размытый все окно.overlay(CSS) 
+// класс .hidden со значением { display: none; } т.е. ЭЛ. не отображается необходимо удалить-окно отобразиться
+for (let value of btnOpenMod) { // перебирание псевдокол. при помощи for-of====в let value-вкладываются все значения каждого ЭЛ. из псевдоКол.
+    value.addEventListener("click", function () { // назначаем событие"click"-для запуска функции-совершает действие над нашими классами 
+        overlay.classList.toggle("hidden"); // у classList есть метод .toggle(удаляет или добавляет несуществующий класс)
+        modWin.classList.toggle("hidden");
+    }); // при нажатие кн.отобразилось окно
+}
+// далее(при нажатие на крестик небходимо мод.окно закрыть)
+btnCloseMod.addEventListener("click", function() { // назначаем событие"click"-для запуска функции-совершает действие
+    overlay.classList.toggle("hidden"); // у classList есть метод .toggle(добавляет несуществующий класс или удаляет)
+    modWin.classList.toggle("hidden"); // добавляем(возвращаем)класс"hidden"к элементам-display: none;-ЭЛ.не отображаются
+})
+// далее(при нажатие на свободное место на оне небходимо мод.окно закрыть)
+overlay.addEventListener("click", function() { // назначаем событие"click"-для запуска функции-совершает действие
+    overlay.classList.toggle("hidden"); // у classList есть метод .toggle(добавляет несуществующий класс или удаляет)
+    modWin.classList.toggle("hidden"); // добавляем(возвращаем)класс"hidden"к элементам-display: none;-ЭЛ.не отображаются
+})
+// создании клавиши для закрытия мод.окна
+document.addEventListener("keydown", function(event) { // создания события при нажатие кнопки
+    if (event.key == "Escape" && !modWin.classList.contains("hidden")) {
+        overlay.classList.toggle("hidden");
+        modWin.classList.toggle("hidden");
+    }
+
+})                           
+
+
+
